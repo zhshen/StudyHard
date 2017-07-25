6ca9081513ee9b2a396ec1fe5903565aa9c89c9b
Zheng Shen <Zheng Shen>
2017-07-25 15:54:07 +0800

    Create Repository

---
+++ AngularJS/quickstart/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
+import { FormsModule }          from '@angular/forms';
+import { HttpModule }           from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { AppComponent }         from './app.component';
+import { HeroService }          from './hero.service';
+import { DashboardComponent }   from './dashboard.component';
+import { AppRoutingModule }     from './app-routing.module';
+import { HeroSearchComponent }  from './hero-search.component';
+
+@NgModule({
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroSearchComponent
+  ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ HeroService ]
+})
+export class AppModule { }
